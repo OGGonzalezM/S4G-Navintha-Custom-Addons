ca709394f5a9e7a36abaeba3eb414667d1e64007
root <root@localhost.localdomain>
2017-08-08 15:04:51 +0000

    Adding all the modules

---
+++ addons/pruebas_datos_y_menu/models/navintha_perfilcompania.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from openerp import fields, models
+
+class Navintha_datosempresa(models.Model):
+    _name = 'company.profile'
+
+    name = fields.Char(required=True, string="Nombre de la compañia")
+    x_navintha_mision = fields.Text(string="Misión")
+    x_navintha_vision = fields.Text(string="Visión")
+    x_navintha_valores = fields.Text(string="Valores")
+    x_navintha_politicadecalidad = fields.Text(string="Política de Calidad")
+    x_navintha_objetivosdecalidad = fields.Text(string="Objetivos de Calidad")
